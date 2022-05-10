51151ca46c6ac1d95630bb98ad424aa77e4d9f28
claudiozam <claudio.zanoszczyk@honou.com.ar>
2022-05-10 10:52:30 -0300

    cambios commit1

--- karma.conf.js
+++ karma.conf.js
@@ -1,12 +1,12 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-
+// Cambios commit1
 module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
-      require('karma-jasmine'),
+      require('karma-jasmine-commit1'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
